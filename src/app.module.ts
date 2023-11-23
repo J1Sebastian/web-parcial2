065d758d5360fd3709819dbbaf7e3df9da30a0cc
Juan Nunez <j.nunezc@uniandes.edu.co>
2023-11-23 17:37:09 -0500

    feat: readme y package.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'parcial2',
+    entities: [],
+    dropSchema: true,
+    synchronize: true,
+    keepConnectionAlive: true
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/shared/testing-utils/typeorm-testing-config.ts
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+export const TypeOrmTestingConfig = () => [
+ TypeOrmModule.forRoot({
+   type: 'sqlite',
+   database: ':memory:',
+   dropSchema: true,
+   entities: [],
+   synchronize: true,
+   keepConnectionAlive: true
+ }),
+ TypeOrmModule.forFeature([]),
+];
